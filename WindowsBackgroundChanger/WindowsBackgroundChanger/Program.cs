f3cf9c4e5953235c5f83d63793af836c73282326
Programazing <chris@thatamazingprogrammer.com>
2021-01-25 13:23:13 -0500

    Add basic Test Project

--- WindowsBackgroundChanger/WindowsBackgroundChanger/Program.cs
+++ WindowsBackgroundChanger/WindowsBackgroundChanger/Program.cs
@@ -2,11 +2,12 @@
 
 namespace WindowsBackgroundChanger
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
 }
