98477b6ea07fd1185f81c0b9a4d4642d37c75513
Programazing <chris@thatamazingprogrammer.com>
2021-01-25 13:13:03 -0500

    Add Basic Console App

---
+++ WindowsBackgroundChanger/WindowsBackgroundChanger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsBackgroundChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
